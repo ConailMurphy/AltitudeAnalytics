bc7fb163ae1917423c14977ae774ddf9bafd170e
Conail Murphy <conailm27@gmail.com>
2017-07-18 16:51:54 +0100

    reducer.py now writes to s3 need to have another look at mapper to see why it returns so few results

--- mapper.py
+++ mapper.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import sys
-import re
 
 pattern = "https://"
 
@@ -15,27 +14,54 @@ def main(separator='\t'):
     criteria = open("criteria.txt").read().splitlines()
     data = read_input(sys.stdin)
     for words in data:
-        for word in words:
-            if re.match(pattern, word):
-                components = word.split("/")
+        for url in words:
+            if url.startswith(pattern):
+                components = url.split("/")
                 if len(components) == 4:
                     for c in criteria:
-                        if components[3].endswith(c) or components[3].endswith(c + "/") \
-                                or components[3].endswith(c + ".html") or components[3].endswith(c + ".php"):
-                            print '%s%s%s' % (word, separator, c)
-                            break
+                        if components[3].strip("^M").startswith(c):
+                            if components[3].strip("^M").endswith(c):
+                                print '%s%s%s' % (url.strip("^M"), separator, c)
+                                break
+                            elif components[3].strip("^M").endswith(c + "/"):
+                                print '%s%s%s' % (url.strip("^M"), separator, c)
+                                break
+                            elif components[3].strip("^M").endswith(c + ".html"):
+                                print '%s%s%s' % (url.strip("^M"), separator, c)
+                                break
+                            elif components[3].endswith(c + ".php"):
+                                print '%s%s%s' % (url.strip("^M"), separator, c)
+                                break
                 elif len(components) == 5:
                     for c in criteria:
-                        if components[4].endswith(c) or components[4].endswith(c + "/") \
-                                or components[4].endswith(c + ".html") or components[4].endswith(c + ".php"):
-                            print '%s%s%s' % (word, separator, c)
-                            break
+                        if components[4].strip("^M").startswith(c):
+                            if components[4].strip("^M").endswith(c):
+                                print '%s%s%s' % (url.strip("^M"), separator, c)
+                                break
+                            elif components[4].strip("^M").endswith(c + "/"):
+                                print '%s%s%s' % (url.strip("^M"), separator, c)
+                                break
+                            elif components[4].strip("^M").endswith(c + ".html"):
+                                print '%s%s%s' % (url.strip("^M"), separator, c)
+                                break
+                            elif components[4].strip("^M").endswith(c + ".php"):
+                                print '%s%s%s' % (url.strip("^M"), separator, c)
+                                break
                 elif len(components) == 6:
                     for c in criteria:
-                        if components[5].endswith(c) or components[5].endswith(c + "/") \
-                                or components[3].endswith(c + ".html") or components[5].endswith(c + ".php"):
-                            print '%s%s%s' % (word, separator, c)
-                            break
+                        if components[5].strip("^M").startswith(c):
+                            if components[5].strip("^M").endswith(c):
+                                print '%s%s%s' % (url.strip("^M"), separator, c)
+                                break
+                            elif components[5].strip("^M").endswith(c + "/"):
+                                print '%s%s%s' % (url.strip("^M"), separator, c)
+                                break
+                            elif components[5].strip("^M").endswith(c + ".html"):
+                                print '%s%s%s' % (url.strip("^M"), separator, c)
+                                break
+                            elif components[5].strip("^M").endswith(c + ".php"):
+                                print '%s%s%s' % (url.strip("^M"), separator, c)
+                                break
 
 if __name__ == "__main__":
     main()
--- reducer.py
+++ reducer.py
@@ -2,7 +2,13 @@
 
 from itertools import groupby
 from operator import itemgetter
+import boto3
 import sys
+
+AWS_ACCESS_KEY_ID = "AKIAIQWIVEDREFE6BURA"
+AWS_SECRET_ACCESS_KEY = "t9bq0jkefSMt7Oa0uRJzEHEjvPtz7zyZ3QtD0zMS"
+bucket = "common-crawl-extracted-urls"
+filename = "sample1.txt"
 
 
 def read_mapper_output(filename, separator='\t'):
@@ -12,12 +18,16 @@ def read_mapper_output(filename, separator='\t'):
 
 def main(separator='\t'):
     data = read_mapper_output(sys.stdin, separator=separator)
-    for current_word, group in groupby(data, itemgetter(0)):
+    urls = []
+    for current_url, group in groupby(data, itemgetter(0)):
         try:
-            total_count = sum(int(count) for current_word, count in group)
-            print "%s%s%d" % (current_word, separator, total_count)
+            urls.append(current_url + "\n")
         except ValueError:
             pass
+    # write list to file on s3
+    s3 = boto3.resource("s3")
+    s3.Bucket(bucket).put_object(Key=filename, Body="".join(urls))
+
 
 if __name__ == "__main__":
     main()
